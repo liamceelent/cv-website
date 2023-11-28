9b4f273d8cd1d70fd217fffe2af9c9292a34db5e
liamceelent <liamceelent@gmail.com>
2023-11-28 22:18:18 +1300

    started work

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,63 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComp from './components/ParentComp';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import StyleSheet from './components/StyleSheet';
-import InLine from './components/InLine';
-import './appStyles.css';
-import styles from './appStyles.module.css';
-import Form from './components/Form';
-import LifeCycleA from './components/LifeCycleA';
-import FragementDemo from './components/FragementDemo';
-import Table from './components/Table';
-import PureComp from './components/PureComp';
-import RefsDemo from './components/RefsDemo';
-import PortalDemo from './components/PortalDemo';
-import ClickCounter from './components/ClickCounter';
-import HoverCounter from './components/HoverCounter';
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
+import PreviousProjects from "./pages/PreviousProjects";
+import Navbar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      {/* <Greet name = "bruce" size ="tall">
-        <p> this is cool</p>
-      </Greet>
-      <Greet name = "tim" size ="small">
-      <p> this is sick</p>
-      </Greet>
-      <Greet name = "john" size ="meduim">
-      <button> this is </button>
-      </Greet>
-      <Welcome name = "john" size ="meduim"/> */}
-      {/* <Message></Message> */}
-      {/* <Counter></Counter> */}
-      {/* <FunctionClick></FunctionClick> */}
-      {/* <ClassClick></ClassClick> */}
-      {/* <EventBind></EventBind> */}
-      {/* <ParentComp></ParentComp> */}
-      {/* <UserGreeting></UserGreeting> */}
-      {/* <StyleSheet primary = {true}></StyleSheet> */}
-      {/* <InLine></InLine> */}
-      {/* <h1 className='error'> error</h1>
-      <h1 className={styles.success}> success</h1> */}
-      {/* <Form></Form> */}
-      {/* <LifeCycleA> h</LifeCycleA> */}
-      {/* <FragementDemo></FragementDemo> */}
-      {/* <Table></Table> */}
-      {/* <PureComp></PureComp> */}
-      {/* <RefsDemo></RefsDemo> */}
-      {/* <PortalDemo></PortalDemo> */}
-      <ClickCounter></ClickCounter>
-      <HoverCounter></HoverCounter>
-    </div>
+    <Router>
+        <Navbar />
+        <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/PreviousProjects" element={<PreviousProjects />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ cv/src/pages/PreviousProjects.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class PreviousProjects extends Component {
+  render() {
+    return (
+      <div>PreviousProjects</div>
+    )
+  }
+}
+
+export default PreviousProjects
