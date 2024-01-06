b57c35191938ac204fae49e07c7b4c9d709a22c2
liamceelent <liamceelent@gmail.com>
2024-01-06 22:18:36 +1300

    contact page

--- cv/src/components/contactMe.js
+++ cv/src/components/contactMe.js
@@ -3,7 +3,19 @@ import React, { Component } from 'react'
 class ContactMe extends Component {
   render() {
     return (
-      <div >hsi</div>
+      <div className='wrapper-contact'>
+        <div className='contact-box'>
+          <div className='contact-title'>
+            contact me 
+          </div>
+          <div className='contact-email'>
+            email
+          </div>
+          <div className='contact-message'>
+            message
+          </div>
+        </div>
+      </div>
     )
   }
 }
